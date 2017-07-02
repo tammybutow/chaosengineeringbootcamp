c2044fa2a795f54ceabf17bf6dd639e296a2165a
Tammy Butow <tammy@dropbox.com>
2017-07-01 21:20:11 -0700

    update network latency

--- chaos_latency.sh
+++ chaos_latency.sh
@@ -1,1 +1,7 @@
-TO DO
+#!/bin/bash
+# Script for introducing network latency
+
+# Adds 1500ms +- 550ms of latency to each packet
+tc qdisc add dev eth0 root latency delay 1500ms 550ms
+
+#Add ability to stop - TO DO: Command Line menu
